20fe5cdc6ad8b6d0e4c407c27151ff2fa17ae0a8
Ashraful610 <ashalam610@gmail.com>
2024-02-18 20:24:31 +0600

    connect with repository

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// 1. Convert the following JavaScript array into a TypeScript tuple 
+const userInfo = [101, "Ema", "John", true,  , "2023"];
+const solve : [number , string , string , boolean , undefined,string] = [101, "Ema", "John", true,  , "2023"]
+
