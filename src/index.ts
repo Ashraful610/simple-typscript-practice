8a78c5b3ec28cbf2cd4671755828405c46e1883e
Ashraful610 <ashalam610@gmail.com>
2024-02-19 22:27:13 +0600

    problem 8 is solve

--- src/index.ts
+++ src/index.ts
@@ -139,4 +139,16 @@ function findMaxAndMinNumber(...numbers:number[]):{minimum:number, maximum:numbe
 }
 
 const {minimum , maximum} = findMaxAndMinNumber(...numbersArray)
-console.log(minimum, maximum)
+// console.log(minimum, maximum)
+
+// Problem-8 : Create a TypeScript program that declares a function that takes a string parameter with a literal type of "red", "green", or "blue", and an optional boolean parameter. If the boolean parameter is true, log the string parameter in uppercase. If the boolean parameter is false or not provided, log the string parameter in lowercase.
+
+function transformString (color:'red' | 'green' | 'blue' , condition?:boolean){
+  const text = condition ? color.toUpperCase() : color.toLowerCase()
+  console.log(text)
+}
+
+transformString('red' , true)
+transformString('blue')
+
+
