92ae07147e535f21ea6c41e924e74f4ab999c5c9
Ashraful610 <ashalam610@gmail.com>
2024-02-19 19:33:38 +0600

    problem four is solve

--- src/index.ts
+++ src/index.ts
@@ -58,3 +58,28 @@ const filterProduct =<T extends keyof IProduct> (productArray:IProduct[] , key:T
 const result2 = filterProduct(products ,'price' , 15000)
 // console.log(result2)
 
+
+// Problem-4: Suppose you have an array of tuples, where each tuple represents a product and contains the product name, price, and quantity. Write a TypeScript function that calculates the total cost of all the products in the array, using a generic type for the tuple and a type alias for the array.
+
+
+type ProductTuple<T>= [string , T ,  T]
+type ProductArray = ProductTuple<number>[]
+
+const products2 : ProductArray = [
+    ['Mobile' , 15000 , 2],
+    ['Laptop' , 55000 , 1] ,
+    ['Watch' , 2500 , 3]
+]
+
+
+function calculateTotalCost(products:ProductArray) :number {
+ let totalCost  = 0 ;
+ for(const product of products) {
+    const [, price , quantity] = product
+    totalCost += price * quantity
+ }
+ return totalCost
+}
+
+const total = calculateTotalCost(products2)
+// console.log(total)
