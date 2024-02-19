67586baf0da1a92b9bbeb4b427401772c3b728eb
Ashraful610 <ashalam610@gmail.com>
2024-02-19 17:07:51 +0600

    third problem is solve

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,47 @@ function arrayFunction(array1: number[], array2: number[]):number[]{
 }
 
 const result = arrayFunction([1 , 2 , 3 , 4 , 5] , [2 , 4 , 6 , 7 , 1,44 , 22  , 91 , 9  , 10 , 3 , 24])
-console.log(result)
+// console.log(result)
+
+// Problem-3 : You have an interface for Product, containing the product's id, name, price, and category. You want to filter an array of Products based on a specific criterion and value.
+interface IProduct {
+    id:number,
+    name:string,
+    price:number,
+    category:string
+}
+
+const product1 : IProduct = {
+    id:1,
+    name:'Realme C33',
+    price:15000,
+    category:'Mobile'
+}
+const product2 : IProduct = {
+    id:2,
+    name:'Readmi note 11',
+    price:19000,
+    category:'Mobile'
+}
+const product3 : IProduct = {
+    id:3,
+    name:'Oppo',
+    price:22000,
+    category:'Mobile'
+}
+const product4: IProduct = {
+    id:4,
+    name:'Huwei',
+    price:25000,
+    category:'Mobile'
+}
+const products : IProduct[] = [product1, product2, product3, product4]
+
+const filterProduct =<T extends keyof IProduct> (productArray:IProduct[] , key:T , value:number):IProduct[] => {
+ const newArray = productArray.filter((product) =>product[key] === value )
+ return newArray
+}
+
+const result2 = filterProduct(products ,'price' , 15000)
+// console.log(result2)
+
